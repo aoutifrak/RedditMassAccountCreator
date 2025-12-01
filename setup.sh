ac7f20ada6f738f8ba6ae25f584e2b97b5fbdd33
aoutifrak <amine201017@hotmail.fr>
2025-11-30 19:07:53 -0500

    init

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 # 1. Install dependencies
-sudo apt-get install -y docker.io python3 python3-pip chromium-browser
+sudo apt update -y &&
+apt-get install -y docker.io python3 python3-full chromium xvfb git
 sudo systemctl start docker
 sudo usermod -aG docker $USER
 
-# 2. Setup Python packages
-cd reddit-register-vps
-pip3 install -r requirements.txt
+# add docker to sudoers
+echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/docker" | sudo tee /etc/sudoers.d/$USER-docker
+newgrp docker 
 
-# 3. Edit credentials
-nano config.json  # Set NordVPN email/password
+# clone repo
+git clone https://github.com/aoutifrak/RedditMassAccountCreator.git
+cd RedditMassAccountCreator
+# 2. Setup Python packages
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 
 # 4. Make executable
 chmod +x register.py run.sh stop.sh
 
 # 5. Test single instance
-python3 register.py --instance 1
+# python3 register.py --instance 1
 
 # 6. Run 3 instances in parallel
-./run.sh 3
+./run.sh 2
